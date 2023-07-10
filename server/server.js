a27c460c958cae0af42fcb621d2d852580cdee5f
Aubrey Gunn <agunn369@gmail.com>
2023-07-10 16:05:19 -0500

    added logic to server file for apollo server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,42 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+//const routes = require('./routes');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const startServer = async () => {
 
-app.use(routes);
+  const app = express();
+  const PORT = process.env.PORT || 3001;
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  const server = new ApolloServer ({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+};
+
+startServer();
+
+
